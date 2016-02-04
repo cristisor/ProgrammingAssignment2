2afeb8493d0ec30c2ec841712faa6f341b32d840
Cristian <cristisor_ac@yahoo.com>
2016-02-04 21:05:47 +0200

    Matrix inversion in R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## The function returns a list with the properties to set and get the
+## initial input, and to set and get the cached inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    
+    set <- function(x) {
+        m <<- x
+        inverse <- NULL
+    }
+    
+    get <- function()
+        m
+    
+    setInverse <- function(x) {
+        inverse <<- x
+    }
+    
+    getInverse <- function()
+        inverse
+    
+    list(
+        set = set, get = get,
+        setInverse = setInverse,
+        getInverse = getInverse
+    )
 }
 
 
-## Write a short comment describing this function
-
+## Return the inverse if it exists or compute it and cache it if not
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    
+    inverse <- x$getInverse()
+    if (!is.null(inverse)) {
+        return (inverse)
+    }
+    
+    m <- x$get()
+    identityDim <- prod(dim(m))
+    identity <-
+        matrix(rep.int(1, times = identityDim), nrow = nrow(m), ncol = ncol(m))
+    
+    inverse <- solve(m)
+    x$setInverse(inverse)
+    inverse
 }
